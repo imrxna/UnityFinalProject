3a0f88bd3cfc2306151a1d3c676b1b3195f556c2
imrxna <134999411+imrxna@users.noreply.github.com>
2025-11-14 14:11:39 +0700

    jumpPlayer

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	[Header("Horizontal Movement Setting")]
-	[SerializeField] private Rigidbody2D rb;
-
-
-	[SerializeField] private float walkSpeed = 1;
-
+    [Header("Horizontal Movement Setting")]
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float walkSpeed = 1;
     private float xAxis;
+
+    [Header("Ground Check Setting")]
+    [SerializeField] private float jumpForce = 45;
+    [SerializeField] private Transform groundCheckPoint;
+    [SerializeField] private float groundCheckY = 0.2f;
+    [SerializeField] private float groundCheckX = 0.5f;
+    [SerializeField] private LayerMask whatIsGround;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,14 +26,36 @@ public class PlayerController : MonoBehaviour
     {
         GetInput();
         Move();
+        Jump();
     }
 
     void GetInput()
     {
-          xAxis = Input.GetAxisRaw("Horizontal");
+        xAxis = Input.GetAxisRaw("Horizontal");
     }
     private void Move()
     {
         rb.linearVelocity = new Vector2(walkSpeed * xAxis, rb.linearVelocity.y);
     }
+
+    public bool Grounded()
+    {
+        if (Physics2D.Raycast(groundCheckPoint.position, Vector2.down, groundCheckY, whatIsGround) || Physics2D.Raycast(groundCheckPoint.position + new Vector3(groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround) || Physics2D.Raycast(groundCheckPoint.position + new Vector3(groundCheckY, 0, 0), Vector2.down, groundCheckY, whatIsGround))
+        {
+            return true;
+        }
+        else
+        {
+            return false;   
+        }
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && Grounded())
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce);
+        }
+    }
 }
+
